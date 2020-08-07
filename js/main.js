df013c74a00fb05dba8e004127c0aaefe6b27bdb
Patryk Kowalski <pacislavek@gmail.com>
2020-08-07 21:14:33 +0200

    Wersja wzbogacona o sekcję projektów. Oraz dwa pierwsze przykładowe projekty.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var myNav = document.getElementById('navbar');
 window.onscroll = function () {
   "use strict";
   
-  if (document.body.scrollTop >= 10) {
-    myNav.style.backgroundColor = "rgba(0,0,0,0.8)";
+  if (document.body.scrollTop >= 200) {
+    myNav.style.backgroundColor = "rgba(0,0,0,1)";
   } else {
     myNav.style.backgroundColor = "rgba(0,0,0,0.1)";
   }
