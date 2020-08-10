6117fe0793f856cbe4d31626b5a3d90d0c53c6a4
Patryk Kowalski <pacislavek@gmail.com>
2020-08-10 23:36:07 +0200

    testowa wersja scrollontop+bgopacity

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 var myNav = document.getElementById('navbar');
+var toTop = document.getElementById('scrolltotop');
 
 window.onscroll = function () {
   "use strict";
-  
-  if (document.body.scrollTop >= 200) {
+
+  if (document.body.scrollTop >= 200 && window.innerWidth > 1000) {
     myNav.style.backgroundColor = "rgba(0,0,0,1)";
+    toTop.style.display = "block";
   } else {
-    myNav.style.backgroundColor = "rgba(0,0,0,0.1)";
+
+    if (window.innerWidth < 1000) {
+      myNav.style.backgroundColor = "rgba(0,0,0,1)";
+    } else {
+      myNav.style.backgroundColor = "rgba(0,0,0,0.1)"
+    }
+    toTop.style.display = "none";
   }
-  
+
 };
